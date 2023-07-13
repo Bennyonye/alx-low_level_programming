95b40f915b4d46e65856b7c01dda15b0e098ea22
Bennyonye <benedictigbukolu@gmail.com>
2023-07-13 15:30:54 -0700

    My first commit on c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - Entry point
- * 
- * Return: Always 0 (Success)
+ * main - A program that prints a line with the function puts
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a mutilingual puzzle");
-	return (0);
+puts("\"Programming is like building a mutilingual puzzle");
+return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main -Entry point
- *
- * Return: ALways 0 (Success)
+ * main - A program that print a line using the printf function
+ * Return: 0 (Success)
  */
- int main(void)
+int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
