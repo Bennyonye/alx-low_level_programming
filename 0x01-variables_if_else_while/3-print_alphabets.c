100faea357f96503b929ed1ee8e11497f7493fc6
Bennyonye <benedictigbukolu@gmail.com>
2023-07-14 16:04:46 -0700

    My c code

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -17,7 +15,6 @@ int main(void)
 	{
 		putchar(letter);
 	}	
-	putchar('\n');	
+	putchar('\n');
 	return (0);
 }
-
