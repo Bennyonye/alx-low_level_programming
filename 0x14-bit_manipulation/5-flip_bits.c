7a5943da5963abced39f1f9d013c7f384aac7cf7
Benedict Onye <98492400+Bennyonye@users.noreply.github.com>
2023-10-26 21:13:48 +0100

    flip_bit

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,4 +1,4 @@
-#include "main.h"
+include "main.h"
 
 /**
  * flip_bits - Count the number of bits that need to be flipped
@@ -10,16 +10,16 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    int bit;
-    int count = 0;
+	int bit;
+	int count = 0;
 	/* XOR to find the differing bits. */
-    unsigned long int difference = n ^ m;
-
-    /* Iterate through each bit of the difference. */
-    for (bit = 0; bit < (sizeof(unsigned long int) * 8); bit++)
-    {
-        /* Check if the current bit in the difference is set (1). */
-        if ((difference >> bit) & 1)
+	unsigned long int difference = n ^ m;
+	
+	/* Iterate through each bit of the difference. */
+	for (bit = 0; bit < (sizeof(unsigned long int) * 8); bit++)
+{
+	/* Check if the current bit in the difference is set (1). */
+	if ((difference >> bit) & 1)
         {
             count++;
         }
