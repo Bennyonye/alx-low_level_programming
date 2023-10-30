0c132a643e8f25ee2ed54ad0d4bade5346b5e813
Bennyonye <benedictigbukolu@gmail.com>
2023-10-30 16:25:36 +0100

    requiecat

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,36 +7,39 @@
  *
  * Return: The actual number of letters read and printed, or 0 on failure.
  */
-ssize_t read_textfile(const char *filename, size_t letters) {
+ssize_t read_textfile(const char *filename, size_t letters) 
+{
     int fd, bytes_rrd, bytes_rwr;
     char *buff;
-
+    
     if (filename == NULL)
         return (0);
-
-
+    
     fd = open(filename, O_RDONLY);
+    
     if (fd == -1)
         return (0);
-
+    
     buff = malloc(sizeof(char) * letters);
+    
     if (buff == NULL) {
         close(fd);
         return (0);
     }
-
+    
     bytes_rrd = read(fd, buff, letters);
+    
     if (bytes_rrd == -1) {
         close(fd);
         free(buff);
         return (0);
     }
-
+    
     bytes_rwr = write(STDOUT_FILENO, buff, bytes_rrd);
-
+    
     close(fd);
     free(buff);
-
+    
     if (bytes_rwr < 0 || bytes_rwr != bytes_rrd)
         return (0);
 
