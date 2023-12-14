7aaaf596a6385f3c8679d20ab343e0f04d356fe8
Bennyonye <benedictigbukolu@gmail.com>
2023-12-14 21:49:24 +0100

    C-files

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -p
+wget -p .. https://raw.githubusercontent.com/Bennyonye/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so 
+export LD_PRELOAD="$PWD/../Libhack.so"
